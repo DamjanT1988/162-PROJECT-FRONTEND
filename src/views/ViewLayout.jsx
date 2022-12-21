c7b11509c9981f8aa8755103f98e26aeb4e7fe9c
DamjanT1988 <Damjan_t@hotmail.com>
2022-12-21 21:10:42 +0100

    1

--- src/views/ViewLayout.jsx
+++ src/views/ViewLayout.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import {Outlet} from "react-router-dom";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from "../viewcomponents/Header";
+import Footer from "../viewcomponents/Footer";
 import '../style/main.css';
-import '../style/bootstrap.min.css'
+import '../style/bootstrap.min.css';
 
 const ViewLayout = () => {
   return (
