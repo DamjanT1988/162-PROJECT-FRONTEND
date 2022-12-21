e0ca7aac71c97e9595b58d6c62c99ea560109919
DamjanT1988 <Damjan_t@hotmail.com>
2022-12-21 17:25:05 +0100

    1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,19 +3,29 @@ import { BrowserRouter, Route, Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Start</Link>
-        </li>
-        <li>
-          <Link to="/Add">Add</Link>
-        </li>
-        <li>
-          <Link to="/Edit">Edit</Link>
-        </li>
-      </ul>
-    </nav>
+
+    <header className="row">
+        <div>
+            <br/><br/><br/><br/>
+
+            <nav className="navbar fixed-top navbar-dark bg-dark bg-gradient d-flex justify-content-center">
+
+                <a id="start">
+                    <Link to="/" className="m-1 btn btn-light text-decoration-none">START</Link>
+                </a>
+                <a id="add">
+                    <Link to="/Add" className="m-1 btn btn-light text-decoration-none">ADD</Link>
+                </a>
+                <a id="manage">
+                    <Link to="/Edit" className="m-1 btn btn-light text-decoration-none">EDIT</Link>
+                </a>
+                <a id="logout" className="m-1">
+                    LOGOUT?
+                </a>
+            </nav>
+        </div>
+    </header>
+
   );
 }
 
