2c8c82905ae6184eebf333cb41af268135696e93
DamjanT1988 <Damjan_t@hotmail.com>
2022-12-21 17:52:40 +0100

    1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Link } from "react-router-dom";
-import { Button } from 'react-bootstrap';
 
 function Navbar() {
   return (
@@ -11,18 +10,18 @@ function Navbar() {
 
             <nav className="navbar fixed-top navbar-dark bg-dark bg-gradient d-flex justify-content-center">
 
-                <Button id="start">
+                <a id="start">
                     <Link to="/" className="m-1 btn btn-light text-decoration-none">START</Link>
-                </Button>
-                <Button id="add">
+                </a>
+                <a id="add">
                     <Link to="/Add" className="m-1 btn btn-light text-decoration-none">ADD</Link>
-                </Button>
-                <Button id="manage">
+                </a>
+                <a id="manage">
                     <Link to="/Edit" className="m-1 btn btn-light text-decoration-none">EDIT</Link>
-                </Button>
-                <Button id="logout" className="m-1">
-                LOGOUT!
-                </Button>
+                </a>
+                <a id="logout" className="m-1">
+                    LOGOUT?
+                </a>
             </nav>
         </div>
     </header>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom'
+import '../style/bootstrap.min.css'
 
 /*******************************************************************************************/
 // CHILD COMPONENT: REGISTER
