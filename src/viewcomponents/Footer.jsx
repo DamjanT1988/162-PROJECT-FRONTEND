69563df67d602ab8388d2e55a8d7eccb8371be98
DamjanT1988 <Damjan_t@hotmail.com>
2022-12-23 02:26:08 +0100

    1

--- src/viewcomponents/Footer.jsx
+++ src/viewcomponents/Footer.jsx
@@ -2,10 +2,9 @@ import ReactDOM from 'react-dom/client';
 import logo from '../assets/MGG-logotype.jpg';
 
 function Footer() {
-  return (
-
+    //display logo and text
+    return (
     <div className="text-center font-weight-bolder">
-        
     <img src={logo} className="img-fluid" />
     <br />
     Created by Damjan Tosic @ MIUN 2022
